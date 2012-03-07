ac01d77ef7380b55212c42cb96c5b2ededd34bed
Craig Wickesser <craig@mindscratch.org>
2012-03-06 22:03:58 -0500

    removed 'puts' statements

--- lib/mongoid_indifferent_access.rb
+++ lib/mongoid_indifferent_access.rb
@@ -2,27 +2,18 @@ module Mongoid::Extensions::Hash::IndifferentAccess
   VERSION = "0.0.1"
 
   def self.included(klass)
-    puts "included"
     klass.class_eval do
-      puts "eval class"
       # @override Mongoid::Field
       def self.field(name, options={})
-        puts "defining field: #{name}"
         field = super(name, options)
-        puts "Field: #{field}"
-        puts "\t#{field.inspect}"
 
-        field_type = options.fetch(:type, nil)
-        puts "Field type: #{field_type}, #{field_type == Hash}, #{field_type.class}"
-        if field_type.name == Hash.name
+        field_type =
+        if options.fetch(:type, nil) == Hash
           getter_name = name.is_a?(Symbol) ? name : name.intern
           setter_name = "#{getter_name}=".intern
-          puts "Getter: #{getter_name}, Setter: #{setter_name}"
 
           define_method(getter_name) do
-            puts "getting value 2"
             val = super()
-            puts "\tgot val"
             unless val.nil? || val.is_a?(HashWithIndifferentAccess)
               wrapped_hash = val.with_indifferent_access
               self.send(setter_name, wrapped_hash)
@@ -32,14 +23,11 @@ module Mongoid::Extensions::Hash::IndifferentAccess
           end
 
           define_method(setter_name) do |hash|
-            puts "setting value"
             unless hash.nil? || hash.is_a?(HashWithIndifferentAccess)
               hash = hash.with_indifferent_access
             end
             super(hash)
           end
-        else
-          puts "not a hash, #{options[:type] == Hash}"
         end
         field
       end
