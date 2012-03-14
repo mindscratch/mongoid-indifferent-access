b5c57107199f9608f0787219ae43f131d12a7565
Craig Wickesser <craig@mindscratch.org>
2012-03-13 20:27:30 -0400

    more specs

--- spec/mongoid-indifferent-access/indifferent_access_spec.rb
+++ spec/mongoid-indifferent-access/indifferent_access_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'mongoid_indifferent_access'
 require 'mongoid'
 
+# mocking some of what Mongoid::Document would provide
+# so the spec doesn't require having MongoDB running
 class MockSuper
   attr_accessor :config
   def self.field(name, options={})
@@ -24,8 +26,16 @@ module Mongoid::Extensions::Hash
       g
     }
 
-    it "returns a value for a given key as a String" do
+    it "returns a value given a String as the key" do
       subject.config["value"].should eq(123)
+    end
+
+    it "returns a value given a Symbol as the key" do
+      subject.config[:value].should eq(123)
+    end
+
+    it "returns nil given a non-existing key" do
+      subject.config[:non_existant].should be_nil
     end
 
   end
