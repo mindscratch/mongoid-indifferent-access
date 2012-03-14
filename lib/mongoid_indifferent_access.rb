4ec6dfc33282e7e7f4c1663d74ab5c68fc8ad422
Craig Wickesser <craig@mindscratch.org>
2012-03-13 20:29:31 -0400

    bumped version to v0.0.2

--- lib/mongoid_indifferent_access.rb
+++ lib/mongoid_indifferent_access.rb
@@ -2,7 +2,7 @@ module Mongoid
   module Extensions
     module Hash
       module IndifferentAccess
-        VERSION = "0.0.1"
+        VERSION = "0.0.2"
 
         def self.included(klass)
           klass.class_eval do
