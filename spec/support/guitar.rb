1312e7fa7da49ea86d75f7c8fd0d3f1484f7c380
Craig Wickesser <craig@mindscratch.org>
2012-08-08 20:03:30 -0400

    removed models from spec/support, not sure why but when specs ran on travis-ci it complained about not being able to locate objects such as Guitar, however they worked when running locally. I just put those models directly into the spec and travis-ci is happy again. 3 1/2 hours of sleep last night, so no more effort being put into this tonight :)

--- spec/support/guitar.rb
+++
@@ -1,9 +0,0 @@
-require 'mongoid'
-require 'mongoid_indifferent_access'
-
-class Guitar
-  include Mongoid::Document
-  include Mongoid::Extensions::Hash::IndifferentAccess
-
-  field :config, :type => Hash
-end
--- spec/support/mandalin.rb
+++
@@ -1,2 +0,0 @@
-class Mandalin < Guitar
-end
